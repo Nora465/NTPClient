6bbeb698885dbf89be2e3c811cea2fe4a85c114b
Nora465 <14955673+Nora465@users.noreply.github.com>
2020-12-27 23:39:35 +0100

    Added Respect the ToS of pool.ntp.org
    The Update Interval was set by default to 60 seconds, but the ToS of pool.ntp.org say it need to be 30min at minimum

--- NTPClient.h
+++ NTPClient.h
@@ -18,10 +18,10 @@ class NTPClient {
     unsigned int  _port           = NTP_DEFAULT_LOCAL_PORT;
     long          _timeOffset     = 0;
 
-    unsigned long _updateInterval = 60000;  // In ms
+    unsigned long _updateInterval = 3600000;  // In ms (1 hour : Defined by ToS of pool.ntp.org)
 
-    unsigned long _currentEpoc    = 0;      // In s
-    unsigned long _lastUpdate     = 0;      // In ms
+    unsigned long _currentEpoc    = 0;        // In s
+    unsigned long _lastUpdate     = 0;        // In ms
 
     byte          _packetBuffer[NTP_PACKET_SIZE];
 
